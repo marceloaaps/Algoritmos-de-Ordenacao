eb6b08f7c933a9dd001db695177f4f2cfa78de28
Marcelo Alexandre Alves <114123437+marceloaaps@users.noreply.github.com>
2024-03-15 20:22:03 -0300

    Criada classe Insertion, para adição do Insertion Sort.

---
+++ src/main/java/com/example/trabalhoed2/Insertion.java
@@ -0,0 +1,32 @@
+package com.example.trabalhoed2;
+
+import java.util.ArrayList;
+
+public class Insertion {
+    private ArrayList<Integer> list = new ArrayList<>();
+    private Integer time;
+
+    public Insertion() {
+    }
+
+    public ArrayList<Integer> getList() {
+        return list;
+    }
+
+    public Integer getTime() {
+        return time;
+    }
+
+    public void setTime(Integer time) {
+        this.time = time;
+    }
+
+    public void insertionReader() {
+
+        for (int i = list.size(); i < list.size() + 1; i++) {
+
+
+
+        }
+    }
+}
--- src/main/java/com/example/trabalhoed2/Main.java
+++ src/main/java/com/example/trabalhoed2/Main.java
@@ -14,15 +14,15 @@ import java.util.Arrays;
 
 public class Main extends Application {
 
-    static String path = "C:\\Users\\Marcelo\\Desktop\\textodec.txt";
-    static String path2 = "C:\\Users\\Marcelo\\Desktop\\textocresc.txt";
+    static String path = "C:\\Users\\marce\\OneDrive\\Área de Trabalho\\texto1.txt";
+    static String path2 = "C:\\Users\\marce\\OneDrive\\Área de Trabalho\\texto2.txt";
+    static String path3 = "C:\\Users\\marce\\OneDrive\\Área de Trabalho\\texto3.txt";
+    public static void main(String[] args) throws IOException {
 
-    public static void main(String[] args){
-
-        FileProperties fp = new FileProperties();
+//        FileProperties fp = new FileProperties();
 //        fp.fileWriterCresc(path, 10000);
-//        fp.fileWriterDec(path, 10000);
-//        fp.fileWriterDeo(path, 10000);
+//        fp.fileWriterDec(path2, 10000);
+//        fp.fileWriterDeo(path3, 10000);
 
         launch(args);
     }
@@ -32,8 +32,8 @@ public class Main extends Application {
 
         Bubble bub = new Bubble();
 
-        ArrayList<Integer> listCresc = new ArrayList<>();
-        ArrayList<Integer> listDec = new ArrayList<>();
+        ArrayList<Integer> listCresc;
+        ArrayList<Integer> listDec;
 
         bub.bubbleReader(path);
         listCresc = bub.getList();
@@ -43,7 +43,10 @@ public class Main extends Application {
         listDec = bub.getList();
         long timeDec = bub.getTime();
 
-        ArrayList<Integer> list = bub.getList();
+        bub.bubbleReader(path3);
+        listDec = bub.getList();
+        long timeRandom = bub.getTime();
+
 
         CategoryAxis xAxis = new CategoryAxis();
         xAxis.setLabel("Tamanho do Array");
@@ -53,6 +56,7 @@ public class Main extends Application {
         series1.setName("Tempos de Processamento");
         series1.getData().add(new XYChart.Data("10000 Crescente", timeCresc));
         series1.getData().add(new XYChart.Data("10000 Decrescente", timeDec));
+        series1.getData().add(new XYChart.Data("10000 Randomico", timeRandom));
 
         System.out.println(listCresc);
         System.out.println(listDec);
