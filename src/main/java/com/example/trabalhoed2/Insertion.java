9f6fb5844f9ac4d0efe67a14af7d2017b6eeb82f
Marcelo Alexandre Alves <114123437+marceloaaps@users.noreply.github.com>
2024-03-20 22:11:52 -0300

    Inserido Insertion Sort na main.

--- src/main/java/com/example/trabalhoed2/Insertion.java
+++ src/main/java/com/example/trabalhoed2/Insertion.java
@@ -1,10 +1,13 @@
 package com.example.trabalhoed2;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.ArrayList;
 
 public class Insertion {
     private ArrayList<Integer> list = new ArrayList<>();
-    private Integer time;
+    private long time;
 
     public Insertion() {
     }
@@ -13,24 +16,33 @@ public class Insertion {
         return list;
     }
 
-    public Integer getTime() {
+    public long getTime() {
         return time;
     }
 
-    public void setTime(Integer time) {
+    public void setTime(long time) {
         this.time = time;
     }
 
-    public void insertionSort() {
-        for (int i = 1; i < list.size(); i++) { // Percorre o array
+    public void insertionSort(String path) throws FileNotFoundException {
+
+
+        BufferedReader br = new BufferedReader(new FileReader(path));
+
+        String line;
+
+
+        time = System.nanoTime();
+        for (int i = 1; i < list.size(); i++) {
 
             int value = list.get(i);
             int j = i-1;
-            while (j >= 0 &&  list.get(j) > value ){ // Enquanto value for maior que o valor depois, ele percorre.
+            while (j >= 0 &&  list.get(j) > value ){
                 list.set(j + 1, list.get(j));
                 j = j-1;
             }
             list.set(j + 1, value);
         }
+        time = System.nanoTime() - time;
     }
 }
--- src/main/java/com/example/trabalhoed2/Main.java
+++ src/main/java/com/example/trabalhoed2/Main.java
@@ -30,22 +30,46 @@ public class Main extends Application {
     public void start(Stage stage) throws IOException {
 
         Bubble bub = new Bubble();
+        Insertion ins = new Insertion();
 
-        ArrayList<Integer> listCresc;
-        ArrayList<Integer> listDec;
+        ArrayList<Integer> listCresc, listDec, listDeo, listCrescIns, listDecIns, listDeoIns;
+        ArrayList<ArrayList<Integer>> bubbleList = new ArrayList<>();
 
         bub.bubbleSort(path);
         listCresc = bub.getList();
+        bubbleList.add(listCresc);
         long timeCresc = bub.getTime();
 
         bub.bubbleSort(path2);
         listDec = bub.getList();
+        bubbleList.add(listDec);
         long timeDec = bub.getTime();
 
         bub.bubbleSort(path3);
-        listDec = bub.getList();
+        listDeo = bub.getList();
+        bubbleList.add(listDeo);
         long timeRandom = bub.getTime();
 
+        //------------------------//
+
+        long timeCrescIns = 0;
+        long timeDecIns = 0;
+        long timeDeoIns = 0;
+
+        for (int i = 0; i<=10; i++){
+            ins.insertionSort(path);
+            timeCrescIns = ins.getTime();
+
+            ins.insertionSort(path2);
+            timeDecIns = ins.getTime();
+
+            ins.insertionSort(path3);
+            timeDeoIns += ins.getTime();
+        }
+
+        long timeTotal = timeCrescIns / 10;
+        long timeTotal2 = timeDecIns / 10;
+        long timeTotal3 = timeDeoIns / 10;
 
         CategoryAxis xAxis = new CategoryAxis();
         xAxis.setLabel("Tamanho do Array");
@@ -53,12 +77,16 @@ public class Main extends Application {
         yAxis.setLabel("Tempo de Processamento (Nanosegundos)");
         XYChart.Series<String, Number> series1 = new XYChart.Series<>();
         series1.setName("Tempos de Processamento");
-        series1.getData().add(new XYChart.Data("10000 Crescente", timeCresc));
-        series1.getData().add(new XYChart.Data("10000 Decrescente", timeDec));
-        series1.getData().add(new XYChart.Data("10000 Randomico", timeRandom));
+        series1.getData().add(new XYChart.Data("Bubble Crescente", timeCresc));
+        series1.getData().add(new XYChart.Data("Bubble Decrescente", timeDec));
+        series1.getData().add(new XYChart.Data("Bubble Randomico", timeRandom));
+        series1.getData().add(new XYChart.Data("Insert Crescente", timeTotal));
+        series1.getData().add(new XYChart.Data("Insert Decrescente", timeTotal2));
+        series1.getData().add(new XYChart.Data("Insert Randomico", timeTotal3));
 
-        System.out.println(listCresc);
-        System.out.println(listDec);
+        System.out.println(timeTotal);
+        System.out.println(timeTotal2);
+        System.out.println(timeTotal3);
 
         LineChart<String, Number> lineChart = new LineChart<>(xAxis, yAxis);
         lineChart.getData().add(series1);
